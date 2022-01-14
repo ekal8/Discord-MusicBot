8db47e5f58fa990912c7b0348cd0d4fe5f8f3206
ekal8 <75772252+ekal8@users.noreply.github.com>
2022-01-14 21:35:13 +0800

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -28,9 +28,9 @@ module.exports = {
   //Lavalink
   Lavalink: {
     id: "Main",
-    host: "lavalink.devz.cloud",
+    host: "lavalink.devin-dev.xyz",
     port: 443, // The port that lavalink is listening to. This must be a number!
-    pass: "mathiscool",
+    pass: "lava123",
     secure: true, // Set this to true if the lavalink uses SSL or you're hosting lavalink on repl.it
   },
 
